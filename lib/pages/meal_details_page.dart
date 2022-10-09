4c774fe819739afde76a10a12e11f37d7ff9254b
Hlib Hryshko <glebg27@gmail.com>
2022-10-09 22:52:25 +0200

    finding the selected meal by id, displaying its image and showing the name of the meal in the app bar

--- lib/pages/meal_details_page.dart
+++ lib/pages/meal_details_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/data.dart';
 
 class MealDetailPage extends StatelessWidget {
   static const routeName = '/meal-details';
@@ -8,17 +9,26 @@ class MealDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context)!.settings.arguments as String;
+    final selectedMeal = meals.firstWhere((meal) => meal.id == mealId);
+    
 
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          mealId
+          selectedMeal.title
         ),
       ),
-      body: Center(
-        child: Text(
-          'Details of the meal with an id $mealId'
-        ),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
       ),
     );
   }
