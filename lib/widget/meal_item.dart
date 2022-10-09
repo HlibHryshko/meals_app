8c9cafb0821a5639e2f05c3404e67dc1a2509e0b
Hlib Hryshko <glebg27@gmail.com>
2022-10-09 22:10:28 +0200

    finishing meal item with a stack with a photo and a title, as well as with some additional information at the bottom of the card

--- lib/widget/meal_item.dart
+++ lib/widget/meal_item.dart
@@ -17,6 +17,32 @@ class MealItem extends StatelessWidget {
   final Complexity complexity;
   final Affordability affordability;
 
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+      case Complexity.Challenging:
+        return 'Challenging';
+      case Complexity.Hard:
+        return 'Hard';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+      case Affordability.Pricey:
+        return 'Pricey';
+      case Affordability.Luxurious:
+        return 'Luxurious';
+      default:
+        return 'Unknown';
+    }
+  }
+
   void _selectMeal(){
 
   }
@@ -30,13 +56,13 @@ class MealItem extends StatelessWidget {
           borderRadius: BorderRadius.circular(15),
         ),
         elevation: 4,
-        margin: EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
         child: Column(
           children: [
             Stack(
               children: [
                 ClipRRect(
-                  borderRadius: BorderRadius.only(
+                  borderRadius: const BorderRadius.only(
                     topLeft: Radius.circular(15),
                     topRight: Radius.circular(15),
                   ),
@@ -47,8 +73,76 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+              Positioned(
+                bottom: 20,
+                right: 10,
+                child: Container(
+                  width: 300,
+                  color: Colors.black54,
+                  padding: EdgeInsets.symmetric(
+                    vertical: 5,
+                    horizontal: 10,
+                  ),
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.white,
+                    ),
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                  ),
+                ),
+              )
               ],
-            )
+            ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.schedule
+                    ),
+                    SizedBox(
+                      width: 6,
+                    ),
+                    Text(
+                      '$duration min',
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(
+                        Icons.work
+                    ),
+                    SizedBox(
+                      width: 6,
+                    ),
+                    Text(
+                      complexityText,
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(
+                        Icons.attach_money
+                    ),
+                    SizedBox(
+                      width: 6,
+                    ),
+                    Text(
+                      affordabilityText,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
           ],
         ),
       ),
