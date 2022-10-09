87ae2f3542b2f9fc9abb0cba923fdac353d67634
Hlib Hryshko <glebg27@gmail.com>
2022-10-09 16:35:16 +0200

    adding navigation, when the user taps on the category item at the categories page

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/pages/category_meals_page.dart';
 
 class CategoryItem extends StatelessWidget {
   const CategoryItem({Key? key, required this.title, required this.color}) : super(key: key);
@@ -6,24 +7,37 @@ class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
+  void _selectCategory(BuildContext ctx, ) {
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+      return const CategoryMealsPage(
+
+      );
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(
-          title,
-        style: Theme.of(context).textTheme.headline6,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
         ),
-        borderRadius: BorderRadius.circular(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
       ),
     );
   }
