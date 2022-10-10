d3d395e1edad7d3bfb1994a1c52f272819aaa16b
Hlib Hryshko <glebg27@gmail.com>
2022-10-10 14:34:38 +0200

    creating filters page

---
+++ lib/pages/filters_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FiltersPage extends StatelessWidget {
+  static const routeName = '/filters';
+
+  const FiltersPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Filters'
+        )
+      ),
+    );
+  }
+}
